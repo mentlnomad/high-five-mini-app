18e0f7c798c35b702f15b5b2c276663e74ccc1c8
Mentl Nomad <91564932+mentlnomad@users.noreply.github.com>
2025-07-24 13:50:15 -0600

    Update next.config.js

--- high-five-fresh/next.config.js
+++ high-five-fresh/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
-};
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
